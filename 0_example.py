4ac7c748b7d5f833a282223f8e7d1b3ec20d5b77
molnarmaxim <molnar.maxim@diak.szbi-pg.hu>
2024-11-11 09:46:51 +0100

    🍉🍗🏀

---
+++ 0_example.py
@@ -0,0 +1,40 @@
+honapok = ['január',
+           'február',
+           'március',
+           'április',
+           'május',
+           'június',
+           'július',
+           'augusztus',
+           'szeptember',
+           'október',
+           'november',
+           'december'
+]
+
+print(honapok)
+
+#lista hossza: len()
+print(len(honapok))
+
+#0. indexű elem kiírása
+print(honapok[0])
+
+
+#1. indexű elem kiírása
+print(honapok[1])
+
+#üres lista létrehozása
+szamok = []
+print(f"Számok lista tartalma: {szamok}")
+#elem hozzáadása
+for i in range(1, 11):
+    szamok.append(i) #append-del hozzáadunk elemeket
+print(f"Számok lista tartalma: {szamok}")
+
+print(szamok[2])
+
+#print(szamok[10]) #List index out of range error
+
+#Utolsó elem kilistázása
+print(szamok[-1]) 
---
+++ 1_1_feladat.py
@@ -0,0 +1,9 @@
+nevek = []
+
+folytat = True
+
+while folytat:
+    nev_hozzaadas = input("Kérlek adj meg neveket!\n")
+    nev_hozzaadas.append(str(nevek))
+
+print(nevek) 
